d9368a2d3e5d3b01b4b29eeda737dd448c04a224
jorgecabrera-dev <jorge.cabrera.llanos@gmail.com>
2026-04-15 01:55:41 -0400

    primer commit

---
+++ prueba.py
@@ -0,0 +1,12 @@
+suma = 0
+cantidad = 0
+
+num = int(input('Ingrese un numero (0 para finalizar): '))
+
+while num != 0:
+    suma += num
+    cantidad += 1
+    num = int(input('Ingrese un numero (0 para finalizar): '))
+
+print ('La suma de todos los numeros es:', suma)
+print ('La cantidad de numeros ingresados es:', cantidad) 
