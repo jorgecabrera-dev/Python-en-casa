877eb110dc3f00bacb187218baf75b3f045f452c
Jorge <jorge.cabrera.llanos@gmail.com>
2026-04-16 01:37:22 -0400

    no sé qué estoy haciendo

--- prueba.py
+++ prueba.py
@@ -8,5 +8,5 @@ while num != 0:
     cantidad += 1
     num = int(input('Ingrese un numero (0 para finalizar): '))
 
-print ('La suma de todos los numeros es:', suma)
-print ('La cantidad de numeros ingresados es:', cantidad)
+print('La suma de todos los numeros es:', suma)
+print('La cantidad de numeros ingresados es:', cantidad)
